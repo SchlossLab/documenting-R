414f29b2ff48d030df1aeb98ff37b320fa54e791
Kelly Sovacool <kellysovacool@gmail.com>
2019-05-24 15:10:52 -0400

    Fix data documentation

--- data-raw/baxter_metadata.R
+++ data-raw/baxter_metadata.R
@@ -1,3 +1,4 @@
-## code to prepare `metadata` dataset
+# code to prepare `baxter_metadata` dataset
+library(usethis)
 baxter_metadata <- get_metadata()
-usethis::use_data(baxter_metadata)
+usethis::use_data(baxter_metadata, overwrite = TRUE)
