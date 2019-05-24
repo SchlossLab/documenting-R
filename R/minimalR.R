c0ff58470c7f3cab02be0550cf55e7d6e8acc744
Kelly Sovacool <kellysovacool@gmail.com>
2019-05-24 15:28:38 -0400

    Add Pat & Joanna's description of the package

---
+++ R/minimalR.R
@@ -0,0 +1,10 @@
+#' minimalR: A package to help people learn R using microbial ecology data.
+#'
+#' The minimalR package contains four important functions: get_metadata, get_bmi, get_bmi_category, and is_obese.
+#'
+#' @section minimalR functions:
+#' The minimalR function get_metadata
+#'
+#' @docType package
+#' @name minimalR
+NULL
